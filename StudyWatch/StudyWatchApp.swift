a6d32126c768047c97fd2f3cc83d368808b3fdb9
JoshuaRosado <joshua06rosado@gmail.com>
2025-03-30 14:57:59 -0500

    Initial Commit

---
+++ StudyWatch/StudyWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyWatchApp.swift
+//  StudyWatch
+//
+//  Created by Joshua Rosado Olivencia on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct StudyWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudyWatchTests/StudyWatchTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudyWatchTests.swift
+//  StudyWatchTests
+//
+//  Created by Joshua Rosado Olivencia on 3/30/25.
+//
+
+import Testing
+@testable import StudyWatch
+
+struct StudyWatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
