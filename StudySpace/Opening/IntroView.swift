81cbb8fd7698ee5443807cb419cc56adf00164a3
JoshuaRosado <joshua06rosado@gmail.com>
2025-04-01 15:34:13 -0500

    IntroView updated and timer for opening was added

--- StudySpace/Opening/IntroView.swift
+++ StudySpace/Opening/IntroView.swift
@@ -8,30 +8,43 @@
 import SwiftUI
 
 struct IntroView: View {
+    
+    // tracking when the view appears
     @State private var onAppear = false
     var body: some View {
         ZStack{
-            Color("eucalyptus")
+            // BG color
+            Color("lunarEclipse")
                 .ignoresSafeArea()
             VStack(spacing: 10){
                 Text("Welcome to")
                     .font(.subheadline)
                     .fontWeight(.semibold)
-                    .foregroundStyle(.wholeWheat).opacity(0.8)
+                    .foregroundStyle(.auLait).opacity(0.8)
                     
                 Text("Study Space")
                     .font(.largeTitle)
                     .fontWeight(.black)
-                    .fontDesign(.default)
-                    .foregroundStyle(.wholeWheat)
+                    .fontDesign(.serif)
+                    .foregroundStyle(.cremeBrulee).brightness(0.1)
                 HStack{
+                    // logo
                     Image(systemName: "macbook")
                         .font(.largeTitle)
                     Image(systemName: "timer")
                         .font(.largeTitle)
                 }
+                .foregroundStyle(.auLait)
                 
                 
+            }
+        }
+        // Effects when view appears
+        .scaleEffect(onAppear ? 1: 1.5)
+        .opacity(onAppear ? 1.0: 0.0)
+        .onAppear{
+            withAnimation(.easeOut(duration: 1.0)){
+                onAppear = true
             }
         }
         
--- StudySpace/Opening/OpeningView.swift
+++ StudySpace/Opening/OpeningView.swift
@@ -8,30 +8,38 @@
 import SwiftUI
 
 struct OpeningView: View {
+    // tracking the intro View
+    // When app opens, the introView is visible ( true )
     @State private var introViewVisible = true
+    // time for the introView to last
     @State private var timeRemainig = 5
     
+    // countdown timer
     var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        Text(timeRemainig, format: .number)
-            .onReceive(timer) { _ in
-                if timeRemainig > 0 {
-                    timeRemainig -= 1
-                }
-                
-                else if timeRemainig == 0 {
-                    withAnimation{
-                        introViewVisible = false
+        ZStack{
+            Text(timeRemainig, format: .number)
+                .onReceive(timer) { _ in
+                    if timeRemainig > 0 {
+                        timeRemainig -= 1
+                    }
+                    
+                    else if timeRemainig == 0 {
+                        withAnimation{
+                            timer.upstream.connect().cancel()
+                            introViewVisible = false
+                        }
                     }
                 }
+                .opacity(0)
+            
+            
+            if introViewVisible {
+                IntroView()
+            } else {
+                //            homeView()
             }
-            .opacity(0)
-        
-        if introViewVisible {
-            IntroView()
-        } else {
-//            homeView()
         }
     }
 }
