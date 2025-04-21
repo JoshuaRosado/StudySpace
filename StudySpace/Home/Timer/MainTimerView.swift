5c1c63e5653bbe95799b0bb9ce5b2044c11b5472
JoshuaRosado <joshua06rosado@gmail.com>
2025-04-21 12:38:08 -0500

    Documentation created  for MainTimerView

--- StudySpace/Home/Timer/MainTimerView.swift
+++ StudySpace/Home/Timer/MainTimerView.swift
@@ -9,21 +9,33 @@ import SwiftUI
 import Combine
 
 struct MainTimerView: View {
-    
+    // StudyTime --
     @State var timeRemaining: Int
-    @State var isVisible = true
+    // isVisible is not used for this View
+    @State var isVisible = false
+    // BreakTime --
     @State var breakTimeRemaining: Int
-    @State var timerStarts = false
+    // Tracking when timer Starts
+    @State var breakTimerStarts = false
+    // Transition from studyTime to breakTime sound effect
     let studyTime = "bell_break.wav"
+    // Transition from breakTime to studyTime sound effect
     let breakTime = "studyTimer.wav"
+    
     
     var timer : Publishers.Autoconnect<Timer.TimerPublisher>
     
     
     var body: some View {
-        if !timerStarts {
+        // As long breakTime has NOT started
+        
+        if !breakTimerStarts {
+            
+            // Display TimerView with StudyTime parameters
             VStack{
-                TimerView(timeRemaining: timeRemaining, isVisible: $isVisible, timerStarts: $timerStarts, soundEffectFileName: studyTime, timer: timer)
+                // StudyTime Timer ----
+                
+                TimerView(timeRemaining: timeRemaining, isVisible: $isVisible, timerStarts: $breakTimerStarts, soundEffectFileName: studyTime, timer: timer)
                     .font(.largeTitle)
                 
                 Text("Study Mode")
@@ -31,24 +43,26 @@ struct MainTimerView: View {
                 
             }
         } else {
+            // If breakTime starts
             VStack{
-                TimerView(timeRemaining: breakTimeRemaining, isVisible: $isVisible, timerStarts: $timerStarts,soundEffectFileName: breakTime , timer: timer)
+                // Display TimerView with BreakTime parameters
+                
+                // BreakTime Timer ----
+                
+                TimerView(timeRemaining: breakTimeRemaining, isVisible: $isVisible, timerStarts: $breakTimerStarts,soundEffectFileName: breakTime , timer: timer)
                     .font(.largeTitle)
-
-                Text("Break time")
+                
+                Text("Break Time")
                     .font(.caption)
-                    
+                
             }
             .fontWeight(.black)
             .foregroundStyle(.auLait)
             
         }
-        
     }
-    
-    
 }
 
 #Preview {
-    MainTimerView(timeRemaining: 10, isVisible: true, breakTimeRemaining: 5, timerStarts: false, timer: Timer.publish(every: 1, on: .main, in: .common).autoconnect())
+    MainTimerView(timeRemaining: 10, isVisible: true, breakTimeRemaining: 5, breakTimerStarts: false, timer: Timer.publish(every: 1, on: .main, in: .common).autoconnect())
 }
